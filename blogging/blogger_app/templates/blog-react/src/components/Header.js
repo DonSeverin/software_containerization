ab487dcdc94e8f55922636a0ff0f1c1114e4fa43
Viktor <y.wu14@student.vu.nl>
2024-01-14 23:22:57 +0100

    React frontend init.

---
+++ blogging/blogger_app/templates/blog-react/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-black py-4 flex">
+            <h1 className="text-white text-4xl font-bold">&lt;Kublog /&gt;</h1>
+        </header>
+    );
+};
+
+export default Header;
